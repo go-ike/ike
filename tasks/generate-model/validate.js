eb1e05a1de6937eb1e15ab49b9394b937fdf28af
Mateus Pinheiro <mateusp@br.ibm.com>
2016-11-01 13:45:27 -0200

    Add nesting validation for model generation

--- tasks/generate-model/validate.js
+++ tasks/generate-model/validate.js
@@ -2,9 +2,29 @@ const pluralize = require('pluralize');
 const chalk     = require('chalk');
 const fs        = require('fs');
 const normalize = apprequire('helpers/normalize.js');
+const isIkeRoot    = apprequire('helpers/is-ike-root');
+const isIkeProject = apprequire('helpers/is-ike-project');
 
 function validate(modelName, args) {
 	const ignoreSingularWarning = args.i;
+
+	/**
+	 * SHOULD BE AT PROJECT ROOT
+	 * This commands should only be ran at the
+	 * project root path.
+	 */
+	if(!isIkeProject()) {
+		console.log(chalk.red.bold('Not an ike project'));
+		console.log('Why don\'t you create a new project first?');
+		console.log('$ ike new project-name');
+		return false;
+	}
+
+	if(isIkeProject() && !isIkeRoot()) {
+		console.log(chalk.red.bold('Not at the project root'));
+		console.log('Generation tasks only work from the project root');
+		return false;
+	}
 
 	/**
 	 * NAME SHOULD BE SINGULAR
